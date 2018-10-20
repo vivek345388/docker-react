3d77704979f59b8a90d75f225a3a1e5bdb6c1006
root <root@ip-172-31-13-73.ap-south-1.compute.internal>
2018-10-20 08:59:02 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Jai Sriram!!Jai Sita Matha!!Jai Hanuman!!
           </p>
           <a
             className="App-link"
